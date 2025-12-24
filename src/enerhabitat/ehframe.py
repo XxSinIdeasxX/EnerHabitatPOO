34ad5dec9e68f6fb51bde2e846ffad20d3c6b347
Fer Rodríguez <ferrodriguez2509@gmail.com>
2025-12-23 19:16:40 -0600

    feat: Tsa() atributos por separado

--- src/enerhabitat/ehframe.py
+++ src/enerhabitat/ehframe.py
@@ -74,7 +74,7 @@ class Location:
         f2 = f'{year}-{month}-{day} 23:59'
 
 
-        epw_data = self.__epw_data(year=year)
+        epw_data = self.__epw_format_data(year=year)
 
         dia_promedio = pd.date_range(start=f1, end=f2, freq='1s',tz=self.timezone)
         location = pvlib.location.Location(latitude = self.latitude, 
@@ -106,13 +106,13 @@ class Location:
         Prints Location information.
         """
         print(f'City: {self.city}')
+        print(f'Timezone: {self.timezone}')
         print(f'Latitude: {self.latitude}°')
         print(f'Longitude: {self.longitude}°')
         print(f'Altitude: {self.altitude} m')
-        print(f'Timezone: {self.timezone}')
         print(f'File: {self.epw}')
         
-    def __epw_data(self, year = None, warns = False, alias = True):
+    def __epw_format_data(self, year = None, warns = False, alias = True):
         """
         Reads Location's EPW file and returns a formatted DataFrame.
             year : None default to leave intact the year or change if desired. It raises a warning.
@@ -191,31 +191,27 @@ class Location:
         return data
     
 class System:
-    """_summary_
 
-    Attributes:
-        _type_: _description_
-        
-    Methods:
-        _method_(self, args): _description_
-    """
     
     def __init__(self, location:Location, tilt = 90, azimuth = 0, absortance = 0.8):
         self.tilt = tilt
         self.azimuth = azimuth
         self.absortance = absortance
         self.location = location
+        
+        self.__updated = True
+        self.__tsa_dataframe = None
     
     @property
     def location(self):
-        return self.__inst_location
+        return self.__instance_location
     @location.setter
     def location(self, loc:Location):
         """
         Location object containing climate data.
         """
-        self.__inst_location = loc
-        self.__updated = True
+        self.__instance_location = loc
+        self.__invalidate_cache()
 
     @property
     def tilt(self):
@@ -225,8 +221,9 @@ class System:
         """
         Tilt angle of the surface in degrees.
         """
-        self.__tilt = angle
-        self.__updated = True
+        if angle != getattr(self, "__tilt", None):
+            self.__tilt = angle
+            self.__invalidate_cache()
         
     @property
     def azimuth(self):
@@ -236,5 +233,175 @@ class System:
         """
         Azimuth angle of the surface in degrees.
         """
-        self.__azimuth = angle
+        if angle != getattr(self, "__azimuth", None):
+            self.__azimuth = angle
+            self.__invalidate_cache()
+
+    @property
+    def absortance(self):
+        return self.__absortance
+    @absortance.setter
+    def absortance(self, value:float):
+        """
+        Surface absortance of the system's external material.
+        """
+        if value != getattr(self, "__absortance", None):
+            self.__absortance = value
+            self.__invalidate_cache()
+
+    def Tsa(self,
+            # solar_absortance:float=None,
+            # surface_tilt:float=None,
+            # surface_azimuth:float=None
+            ) -> pd.DataFrame: 
+        """
+        Sun-air temperature per second for the average day experienced
+        by a surface based on a meanDay dataframe from System's Location
+        (Ta, Ig, Ib and Id).
+
+        Returns:
+            DataFrame: Predicted sun-air temperature ( Tsa ) and solar irradiance ( Is )
+            per second for the average day.
+        """
+        """
+        if solar_absortance is not None:
+            self.absortance = solar_absortance
+        if surface_tilt is not None:
+            self.tilt = surface_tilt
+        if surface_azimuth is not None:
+            self.azimuth = surface_azimuth
+        """
+        if self.__tsa_dataframe is None or self.__updated:
+            self.__tsa_dataframe = self.__calc_tsa()  # el método que calcula Tsa
+            self.__updated = False
+        return self.__tsa_dataframe
+    
+    def __calc_tsa(self) -> pd.DataFrame:
+        meanDay_dataframe = self.location.meanDay()
+        absortance = self.absortance
+        tilt = self.tilt
+        azimuth = self.azimuth
+        
+        global ho
+        outside_convection_heat_transfer = ho
+
+        if tilt == 0:
+            LWR = 3.9
+        else:
+            LWR = 0.
+
+        total_irradiance = pvlib.irradiance.get_total_irradiance(
+            surface_tilt=tilt,
+            surface_azimuth=azimuth,
+            dni=meanDay_dataframe['Ib'],
+            ghi=meanDay_dataframe['Ig'],
+            dhi=meanDay_dataframe['Id'],
+            solar_zenith=meanDay_dataframe['zenith'],
+            solar_azimuth=meanDay_dataframe['azimuth']
+        )
+
+        # Add Is
+        meanDay_dataframe['Is'] = total_irradiance.poa_global
+
+        # Add Tsa
+        meanDay_dataframe['Tsa'] = meanDay_dataframe.Ta + meanDay_dataframe.Is*absortance/outside_convection_heat_transfer - LWR
+
+        return meanDay_dataframe
+    
+    def solveCS(
+        constructive_system:list,
+        Tsa_dataframe:pd.DataFrame,
+        AC = False,
+        energia=False
+        )->pd.DataFrame:
+        """
+        Solves the constructive system's inside temperature with the Tsa simulation dataframe.
+
+        Args:
+            constructive_system (list): List of tuples from outside to inside with material and width.
+            Tsa_dataframe (DataFrame): Predicted sun-air temperature ( Tsa ) per second for the average day DataFrame.
+
+        Returns:
+            Ti (DataFrame): Interior temperature for the constructive system.
+            ET (float): Energy transfer if energia=True.
+            Qcool, Qheat (float): Cooling energy and heating energy values if AC=True.
+        """
+
+        global La     # Length of the dummy frame
+        global Nx     # Number of elements to discretize
+        global ho     # Outside convection heat transfer
+        global hi     # Inside convection heat transfer
+        global dt     # Time step
+
+        global AIR_DENSITY
+        global AIR_HEAT_CAPACITY
+
+        SC_dataframe = Tsa_dataframe.copy()
+
+        propiedades = materials_dict()
+
+        cs = set_construction(propiedades, constructive_system)
+        k, rhoc, dx = set_k_rhoc(cs, Nx)
+        mass_coeff, a_static, b_static, c_static = prepare_static_coefficients(k, rhoc, dx, dt, ho, hi)
+
+        d = np.empty(Nx)
+        P = np.empty(Nx)
+        Q = np.empty(Nx)
+        Tn_aux = np.empty(Nx)
+        capacitance_factor = hi * dt / (AIR_DENSITY * AIR_HEAT_CAPACITY * La)
+
+        T = np.full(Nx, SC_dataframe.Tn.mean())
+        SC_dataframe['Ti'] = SC_dataframe.Tn.mean()
+
+        SC_dataframe = SC_dataframe.iloc[::dt]
+        Tsa_vals = SC_dataframe['Tsa'].to_numpy()
+        Ti_vals = SC_dataframe['Ti'].to_numpy()
+        Ti_new = np.empty_like(Ti_vals)
+        n_steps = Tsa_vals.shape[0]
+
+        C = 1
+        ET = 0.0
+
+        if AC:  # AC = True
+            while C > 5e-4: 
+                Told = T.copy()
+                Qcool = Qheat = 0.
+                for idx in range(n_steps):
+                    calculate_coefficients(mass_coeff, T, Tsa_vals[idx], ho, Ti_vals[idx], hi, d)
+                    # Llamado de funcion para Acc
+                    T, Ti = solve_PQ_AC(a_static, b_static, c_static, d, T, Nx, Ti_vals[idx], hi, La, dt)
+                    if (T[Nx-1] > Ti):
+                        Qcool += hi*dt*(T[Nx-1]-Ti)
+                    if (T[Nx-1] < Ti):
+                        Qheat += hi*dt*(Ti-T[Nx-1])
+                    Ti_vals[idx] = Ti
+                Tnew = T.copy()
+                C = abs(Told - Tnew).mean()
+
+            SC_dataframe['Ti'] = Ti_vals
+
+            return SC_dataframe['Ti'], Qcool, Qheat
+
+        else:
+            ET = 0.0
+            while C > 5e-4: 
+                Told = T.copy()
+                ET_iter = 0.
+                for idx in range(n_steps):
+                    tint_prev = Ti_vals[idx]
+                    calculate_coefficients(mass_coeff, T, Tsa_vals[idx], ho, tint_prev, hi, d)
+                    T, tint_new = solve_PQ(a_static, b_static, c_static, d, T, Nx, tint_prev, capacitance_factor, P, Q, Tn_aux)
+                    Ti_new[idx] = tint_new
+                    if T[Nx-1] > tint_new:
+                        ET_iter += hi * (T[Nx - 1] - tint_new) * dt
+                Ti_vals[:] = Ti_new
+                C = np.abs(Told - T).mean()
+                ET = ET_iter
+
+            SC_dataframe['Ti'] = Ti_vals
+
+            if energia: return SC_dataframe['Ti'], ET
+            else: return SC_dataframe['Ti']
+
+    def __invalidate_cache(self):
         self.__updated = True
